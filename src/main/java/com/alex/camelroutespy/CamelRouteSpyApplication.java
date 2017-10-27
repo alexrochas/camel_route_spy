e8cd7a28db809faa4629ab8f9501d6238843771e
Alex Rocha <alex.rochas@yahoo.com.br>
2017-10-26 23:34:31 -0200

    refactor: just tabs to spaces

--- src/main/java/com/alex/camelroutespy/CamelRouteSpyApplication.java
+++ src/main/java/com/alex/camelroutespy/CamelRouteSpyApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CamelRouteSpyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CamelRouteSpyApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CamelRouteSpyApplication.class, args);
+    }
 }
